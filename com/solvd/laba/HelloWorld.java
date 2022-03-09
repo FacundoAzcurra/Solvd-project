a59e5aefb6ccfd54ad04ebb910dd5f76f68117a5
Facundo Azcurra Rech <fazcurra3@gmail.com>
2022-03-09 18:59:05 -0300

    Fix

--- com/solvd/laba/HelloWorld.java
+++ com/solvd/laba/HelloWorld.java
@@ -1,8 +1,12 @@
 package com.solvd.laba;
+import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main(String args[]) {
-        System.out.println("Hello " + args[0] + " " + args[1]);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " +name);
     }
 }
 
