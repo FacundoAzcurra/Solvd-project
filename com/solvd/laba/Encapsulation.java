4100ec512ae44ce97f82a2342cf91e919ed7b6e5
Facundo Azcurra Rech <fazcurra3@gmail.com>
2022-03-13 23:28:18 -0300

    created Encapsulation and Plane

---
+++ com/solvd/laba/Encapsulation.java
@@ -0,0 +1,10 @@
+package com.solvd.laba;
+
+public class Encapsulation {
+    public static void main(String[] args) {
+
+Plane plane = new Plane ("Boeing", "747", 2019);
+
+System.out.println(plane.getMake());
+    }
+}
---
+++ com/solvd/laba/Plane.java
@@ -0,0 +1,23 @@
+package com.solvd.laba;
+
+public class Plane {
+    private String make;
+    private String model;
+    private int year;
+
+    Plane(String make, String model, int year) {
+
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+ public String getMake(){
+        return make;
+    }
+public String getModel(){
+        return model;
+}
+public int getYear(){
+        return year;
+}
+}
