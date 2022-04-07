1e3f8d312dd8981b30d54adbdbbcc8d2ba7898fd
Facundo Azcurra Rech <fazcurra3@gmail.com>
2022-04-07 08:31:38 -0300

    Enums created and added one generic.

---
+++ main/java/com/solvd/airportpackage/model/constant/Location.java
@@ -0,0 +1,8 @@
+package com.solvd.airportpackage.model.constant;
+
+public enum Location {
+    CITY,
+    COUNTRY,
+    DISTANCE_FROM_CAPITAL_CITY,
+
+}
---
+++ main/java/com/solvd/airportpackage/model/constant/PlaneColors.java
@@ -0,0 +1,8 @@
+package com.solvd.airportpackage.model.constant;
+
+public enum PlaneColors {
+    BLUE(1),RED(2),WHITE(3);
+
+    PlaneColors(int i) {
+    }
+}
